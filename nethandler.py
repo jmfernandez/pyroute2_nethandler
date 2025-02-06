605d455f1ddbba09f36c0fe46b3c470ea1ff2e87
José Mª Fernández <josemariafg@gmail.com>
2025-02-06 18:24:08 +0100

    Implemented the rest of features

--- nethandler.py
+++ nethandler.py
@@ -2,11 +2,20 @@
 
 import datetime
 import pprint
+import os
 import queue
 import re
+import resource
+import signal
+import stat
+import sys
 import time
 
 import pyroute2
+
+from xdg_base_dirs import (
+    xdg_config_home,
+)
 
 # https://man7.org/linux/man-pages/man7/rtnetlink.7.html
 
@@ -41,54 +50,108 @@ IFA_F_NOPREFIXROUTE = 0x200
 IFA_F_MCAUTOJOIN = 0x400
 IFA_F_STABLE_PRIVACY = 0x800
 
+CONFIG_SUBDIR = "pyroute2_nethandler"
+
+def get_maximum_file_descriptors() -> "int":
+    """Get the maximum number of open file descriptors for this process.
+
+    :return: The number (integer) to use as the maximum number of open
+        files for this process.
+
+    The maximum is the process hard resource limit of maximum number of
+    open file descriptors. If the limit is “infinity”, a default value
+    of ``MAXFD`` is returned.
+    """
+    (__, hard_limit) = resource.getrlimit(resource.RLIMIT_NOFILE)
+
+    result = hard_limit
+    if hard_limit == resource.RLIM_INFINITY:
+        result = MAXFD
+
+    return result
+
+def cldhandler(signum, frame):
+	while True:
+		try:
+			pid, exit_status = os.wait()
+			print(f"{pid} {os.waitstatus_to_exitcode(exit_status)}")
+		except ChildProcessError as cpe:
+			break
+
 # Create Instance of IPDB
 q = queue.Queue()
 
-with pyroute2.IPDB() as ipdb:
-	action = 'RTM_NEWLINK'
-	def cb(ipdb, msg, action):
+def cb(ipdb, msg, action):
+	# These are the only actions being attended
+	# see https://man7.org/linux/man-pages/man7/rtnetlink.7.html
+	if action in ("RTM_NEWADDR","RTM_DELADDR", "RTM_GETADDR"):
 		index = msg.get('index')
 		if index is not None:
 			interface = ipdb.interfaces[index]
+			interface_name = interface.ifname
 		else:
 			interface = None
-		q.put((datetime.datetime.now(), action, msg, interface))
+			interface_name = '(unset)'
+
+		# events from ignorable interfaces are filtered out
+		do_queue = True
+
+		if interface_name in IFACE_IGNORABLE:
+			do_queue = False
+		else:
+			for pat in IFACE_IGNORABLE_RE:
+				if pat.search(interface_name):
+					do_queue = False
+					break
+
+		if do_queue:
+			q.put((datetime.datetime.now(), action, msg, interface))
+
+with pyroute2.IPDB() as ipdb:
+	action = 'RTM_NEWLINK'
 
 	ipdb.register_callback(cb, mode='post')
+	
+	signal.signal(signal.SIGCLD, cldhandler)
 
 	while(True):
 		stamp, action, msg, interface = q.get()
-		if action not in ("RTM_NEWADDR","RTM_DELADDR", "RTM_GETADDR"):
-			continue
-
-		if interface is not None:
-			interface_name = interface.ifname
-		else:
-			interface_name = '(unset)'
-
-		if interface_name in IFACE_IGNORABLE:
-			continue
-		do_ignore = False
-		for pat in IFACE_IGNORABLE_RE:
-			if pat.search(interface_name):
-				do_ignore = True
-				break
-		if do_ignore:
-			continue
 		
-		# msg is an instance of ifaddrmsg
-		# msg['attrs'] is a list of instances of nla_slot
-		print(f"{stamp.isoformat()} {interface_name} {action}")
-		is_permanent = False
-		is_addr_set = False
-		for nla_attr in msg["attrs"]:
-			if nla_attr.name == "IFA_FLAGS" and (nla_attr.value & IFA_F_PERMANENT) > 0:
-				is_permanent = True
-			elif nla_attr.name == "IFA_LOCAL":
-				is_addr_set = True
+		base_config_dir = xdg_config_home() / CONFIG_SUBDIR / action
+		
+		# If the directory does not exist, ignore the event!
+		if base_config_dir.is_dir():
+			if interface is not None:
+				interface_name = interface.ifname
+			else:
+				interface_name = '(unset)'
 
-		if is_permanent and is_addr_set:
-			pprint.pprint(msg)
-			
-		#if interface is not None:
-		#	pprint.pprint(interface)
+			# msg is an instance of ifaddrmsg
+			# msg['attrs'] is a list of instances of nla_slot
+			print(f"{stamp.isoformat()} {interface_name} {action}")
+			is_permanent = False
+			is_addr_set = False
+			for nla_attr in msg["attrs"]:
+				if nla_attr.name == "IFA_FLAGS" and (nla_attr.value & IFA_F_PERMANENT) > 0:
+					is_permanent = True
+				elif nla_attr.name == "IFA_LOCAL":
+					is_addr_set = True
+
+			if is_permanent and is_addr_set:
+				for possible_script in base_config_dir.iterdir():
+					if possible_script.is_file() and possible_script.stat().st_mode & (stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH):
+						job_id = os.fork()
+						if job_id == 0:
+							os.setsid()
+							os.closerange(0, get_maximum_file_descriptors())
+							
+							os.execl(possible_script, possible_script, interface_name)
+							# Unreachable code
+							sys.exit(1)
+						elif job_id > 0:
+							print(f"{possible_script} => {job_id}")
+						
+				pprint.pprint(msg)
+				
+			#if interface is not None:
+			#	pprint.pprint(interface)
